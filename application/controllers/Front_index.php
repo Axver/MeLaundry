7f2d37716f12758c5e78a6c5f05b3510494679b7
unknown <ecimoramora@gmail.com>
2019-07-06 12:40:44 +0700

    Frontend added

---
+++ application/controllers/Front_index.php
@@ -0,0 +1,16 @@
+<?php 
+        
+defined('BASEPATH') OR exit('No direct script access allowed');
+        
+class Front_index extends CI_Controller {
+
+public function index()
+{
+                $this->load->view('front_end/front_index');
+}
+        
+}
+        
+    /* End of file  front_index.php */
+        
+                            
--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -24,11 +24,18 @@
             <li class="header">MAIN NAVIGATION</li>
             <li class="treeview">
                 <a href="#">
-                    <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
+                    <i class="fa fa-database"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
                 </a>
                 <ul class="treeview-menu">
-                    <li><a href="<?php echo site_url('dashboard1') ?>"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-                    <li><a href="<?php echo site_url('dashboard2') ?>"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+                    <li><a href="<?php echo site_url('antar_jemput') ?>"><i class="fa fa-motorcycle"></i>Antar Jemput</a></li>
+                    <li><a href="<?php echo site_url('barang_transaksi') ?>"><i class="fa fa-money"></i>Transaksi</a></li>
+                    <li><a href="<?php echo site_url('diskon') ?>"><i class="fa fa-cut"></i>Diskon</a></li>
+                    <li><a href="<?php echo site_url('driver') ?>"><i class="fa fa-car"></i>Driver</a></li>
+                    <li><a href="<?php echo site_url('driver_antar_jemput') ?>"><i class="fa fa-info-circle"></i>Driver Status</a></li>
+                    <li><a href="<?php echo site_url('fasilitas') ?>"><i class="fa fa-bars"></i>Fasilitas</a></li>
+                    <li><a href="<?php echo site_url('fasilitas_laundry') ?>"><i class="fa fa-info-circle"></i>Fasilitas Laundry</a></li>
+                    <li><a href="<?php echo site_url('jenis_barang') ?>"><i class="fa fa-bullseye "></i>Jenis Barang</a></li>
+                    <li><a href="<?php echo site_url('lokasi_laundry') ?>"><i class="fa fa-briefcase"></i>Laundry</a></li>
                 </ul>
             </li>
         </ul>
